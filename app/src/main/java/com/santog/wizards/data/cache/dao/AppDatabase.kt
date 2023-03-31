fe45aa67293cda73e5f153f5e530f2f028cf3c13
Santo <77076220+Santo-G@users.noreply.github.com>
2023-03-31 09:20:22 +0200

    fix: minor fixes

--- app/src/main/java/com/santog/wizards/data/cache/dao/AppDatabase.kt
+++ app/src/main/java/com/santog/wizards/data/cache/dao/AppDatabase.kt
@@ -1,59 +1,12 @@
 package com.santog.wizards.data.cache.dao
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.santog.wizards.data.cache.entities.CharacterEntity
 
 
-@Database(entities = [CharacterEntity::class], version = 1, exportSchema = false) // entities = [Character::class]
+@Database(entities = [CharacterEntity::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun characterDao(): CharacterDAO
-
-    /*companion object {
-        private lateinit var instance: AppDatabase
-
-        fun getInstance(context: Context): AppDatabase {
-            synchronized(this) {
-                if (instance == null) {
-                    instance = Room
-                        .databaseBuilder(
-                            context.applicationContext,
-                            AppDatabase::class.java,
-                            "wizards"
-                        ).fallbackToDestructiveMigration()
-                        .build()
-                }
-
-            }
-            return instance
-        }
-    }
-*/
-
-    /*companion object {
-        @Volatile
-        private var INSTANCE: AppDatabase? = null
-
-        fun getInstance(context: Context): AppDatabase {
-            synchronized(this) {
-                var instance = INSTANCE
-                if (instance == null) {
-                    instance = Room
-                        .databaseBuilder(
-                            context.applicationContext,
-                            AppDatabase::class.java,
-                            "wizards"
-                        ).fallbackToDestructiveMigration()
-                        .build()
-                    INSTANCE = instance
-                }
-                return instance
-            }
-
-        }
-
-    }*/
 }
 
